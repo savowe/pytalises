a1e68bc996df1e1215479953f917c4b7d0c60a7b
Sascha Vowe <vowe@mail.de>
2020-10-19 22:06:17 +0200

    changed setuptools attributes

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup
 
 setup(name='pytalises',
       version='0.1.1',
-      description='',
+      description="""
+      TALISES (This Ain't a LInear Schrödinger Equation Solver) is an easy-to-use Python implementation
+      of the Split-Step Fourier Method, for numeric calculation of a wave function's time-propagation
+      under the Schrödinger equation.""",
       url='http://github.com/savowe/pytalises',
       author='Sascha Vowe',
       author_email='sascha.vowe@posteo.de',
@@ -16,5 +19,13 @@ setup(name='pytalises',
           'pyfftw',
           'numexpr',
       ],
-      zip_safe=False)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.6',
+    zip_safe=False)
 
