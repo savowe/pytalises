7497c26c810c355e7cbc9659ec388ff48dc20311
Sascha Vowe <38558793+savowe@users.noreply.github.com>
2020-10-28 09:57:44 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Sascha Vowe, Bastian Leykauf'
 author = 'Sascha Vowe, Bastian Leykauf'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = '0.1.7'
 
 
 # -- General configuration ---------------------------------------------------
