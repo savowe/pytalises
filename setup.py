08f11dded7730a1b9072bb996d59294a8bb55efb
Sascha Vowe <sascha.vowe@posteo.de>
2020-11-13 01:19:40 +0100

    fixed bug in freely_propagate mthod

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pytalises",
-    version="0.2.3",
+    version="0.2.4",
 
     description="""
       TALISES (This Ain't a LInear Schrödinger Equation Solver) is an easy-to-use Python implementation
