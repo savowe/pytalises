c8d951c356d608d1796d2932c005a5c4b0ebff53
Sascha Vowe <38558793+savowe@users.noreply.github.com>
2020-11-17 22:11:31 +0000

    fixed bug in state_occupation (#15) (#16)

--- tests/wavefunction_test.py
+++ tests/wavefunction_test.py
@@ -1,9 +1,11 @@
 import pytalises as pt
+import numpy as np
 
 
 def test_Wavenfunction():
     # 1D Arguments not as list
-    pt.Wavefunction("exp(-x**2)", (128,), (-1, 1))
+    psi = pt.Wavefunction("exp(-x**2)", (128,), (-1, 1), normalize_const=1.0)
+    np.testing.assert_almost_equal(psi.state_occupation(), 1.0)
     # 1D Arguments as list
     pt.Wavefunction(["exp(-x**2)"], (128,), [(-1, 1)])
     pt.Wavefunction(["exp(-x**2)", "cos(x)"], (128,), [(-1, 1)])
@@ -12,12 +14,14 @@ def test_Wavenfunction():
     pt.Wavefunction("exp(-x**2-y**2)", (128, 128), [(-1, 1), (-1, 1)])
     # 2D Arguments as list
     pt.Wavefunction(["exp(-x**2-y**2)"], (128, 128), [(-1, 1), (-1, 1)])
-    pt.Wavefunction(
+    psi = pt.Wavefunction(
         ["exp(-x**2-y**2)", "c*x**2+y**2"],
         (128, 128),
         [(-1, 1), (-1, 1)],
         variables={"c": 1},
+        normalize_const=666,
     )
+    np.testing.assert_almost_equal(np.sum(psi.state_occupation()), 666)
     # 3D Arguments not as list
     pt.Wavefunction(
         "exp(-x**2-y**2-z**2/d)+d",
