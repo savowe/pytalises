3c5152bd553ac72b0f80704c596156a0dbd9705d
Sascha Vowe <sascha.vowe@posteo.de>
2020-11-25 02:08:06 +0100

    added test for free propagation

--- tests/physics_test.py
+++ tests/physics_test.py
@@ -15,8 +15,7 @@ def test_rabi_oscillations():
     cases = itertools.product(f_Rabi_selection, f_Delta_selection)
 
     for case in cases:
-        f_Rabi = case[0]
-        f_Delta = case[1]
+        f_Rabi, f_Delta = case
         f_g_Rabi = np.sqrt(f_Rabi ** 2 + f_Delta ** 2)  # generalized Rabi frequency
         time = 1 / f_g_Rabi
 
@@ -45,3 +44,40 @@ def test_rabi_oscillations():
             )
             U.potential_prop(delta_t=time / n_steps)
             U.kinetic_prop(delta_t=time / n_steps)
+
+
+def test_free_propagation_of_gaussian_wave_packet():
+    """
+    Tests if a gaussian wave packet disperses and moves
+    as predicted by the Schrödinger equation.
+    """
+    sigma0_select = [0.5, 1, 5]
+    x0_select = [-3.432, 0, 10]
+    k0_select = [-5, 0, 6.2341]
+    hbar = 1.054571817e-34
+    m_select = [hbar, hbar * 2.2]
+
+    cases = itertools.product(sigma0_select, x0_select, k0_select, m_select)
+
+    for case in cases:
+        print(case)
+        sigma0, x0, k0, m = case
+        psi = pt.Wavefunction(
+            ["exp(-((x-x0)/(2*sigma0))**2)*exp(1j*k0*x)"],
+            (32768,),
+            (-200, 200),
+            normalize_const=1.0,
+            variables={"sigma0": sigma0, "x0": x0, "k0": k0},
+            m=m,
+        )
+
+        for _ in range(10):
+            np.testing.assert_almost_equal(
+                psi.var_pos(),
+                sigma0 ** 2 + (hbar ** 2 * psi.t ** 2) / (4 * m ** 2 * sigma0 ** 2),
+                decimal=2,
+            )
+            np.testing.assert_almost_equal(
+                psi.exp_pos(), x0 + hbar / m * k0 * psi.t, decimal=2
+            )
+            psi.freely_propagate(num_time_steps=1, delta_t=1)
